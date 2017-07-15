c16d2dd9f12321acabdd4e71065215172ad1da6a
Li Wang <wangli1426@gmail.com>
2017-07-15 14:14:50 +0800

    added consumer

---
+++ src/main/java/edu/illinois/adsc/Consumer.java
@@ -0,0 +1,69 @@
+package edu.illinois.adsc;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+
+public class Consumer
+{
+    public static void main( String[] args ) throws InterruptedException {
+        String ip = "54.255.217.187";
+        if (args.length == 1) {
+            ip = args[0];
+        }
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, ip + ":9092");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        long start = System.currentTimeMillis();
+        KStreamBuilder builder = new KStreamBuilder();
+
+        System.out.println("begin to get the source.");
+        KStream<String, String> source = builder.stream("streams-file-input");
+        source.print();
+        System.out.println("begin to process the data.");
+        KTable<String, Long> counts = source
+                .flatMapValues(new ValueMapper<String, Iterable<String>>() {
+                    @Override
+                    public Iterable<String> apply(String value) {
+                        return Arrays.asList(value.toLowerCase(Locale.getDefault()).split(" "));
+                    }
+                }).map(new KeyValueMapper<String, String, KeyValue<String, String>>() {
+                    @Override
+                    public KeyValue<String, String> apply(String key, String value) {
+                        return new KeyValue<>(value, value);
+                    }
+                })
+                .groupByKey()
+                .count("Counts");
+
+        // need to override value serde to Long type
+        System.out.println("begin to publish the results.");
+        counts.to(Serdes.String(), Serdes.Long(), "streams-wordcount-outputs");
+        counts.print(Serdes.String(), Serdes.Long());
+
+        KafkaStreams streams = new KafkaStreams(builder, props);
+        streams.start();
+
+        // usually the stream application would be running forever,
+        // in this example we just let it run for some time and stop since the input data is finite.
+//        Thread.sleep(5000L);
+
+        streams.close();
+    }
+}
